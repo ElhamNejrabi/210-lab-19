2cbd63e8ee4b5cd64e96bec3e819cfdda2e93705
Elham Nejrabi <elhamnejrabi@Elhams-MacBook-Pro.local>
2025-10-05 19:48:17 -0700

    third

--- lab19.cpp
+++ lab19.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 struct Review {
@@ -17,7 +21,12 @@ private:
 public:
     Movie(const string& t) : title(t), head(nullptr) {}
 
-    void addReview(double rating, const string& comment) {
+    ~Movie() {
+        clearReviews();
+    }
+
+    void addReview(const string& comment) {
+        double rating = (rand() % 41 + 10) / 10.0; // random 1.0–5.0
         Review* newReview = new Review{rating, comment, head};
         head = newReview;
     }
@@ -38,20 +47,44 @@ public:
             current = current->next;
         }
 
-        if (total > 0) {
-            cout << "    > Average: " << (sum / total) << endl;
-        } else {
-            cout << "    > No reviews.\n";
-        }
+        if (total > 0) cout << "    > Average: " << (sum / total) << endl;
+        else cout << "    > No reviews.\n";
         cout << endl;
+    }
+
+    void clearReviews() {
+        Review* current = head;
+        while (current) {
+            Review* tmp = current;
+            current = current->next;
+            delete tmp;
+        }
+        head = nullptr;
     }
 };
 
 int main() {
+    srand(time(0));
+
+    ifstream infile("reviews.txt");
+    if (!infile) {
+        cerr << "Could not open reviews.txt\n";
+        return 1;
+    }
+
+    vector<string> comments;
+    string line;
+    while (getline(infile, line)) {
+        if (!line.empty()) comments.push_back(line);
+    }
+    infile.close();
+
     Movie m1("Inception");
-    m1.addReview(4.5, "Great visuals");
-    m1.addReview(3.8, "Solid story");
-    m1.addReview(5.0, "Masterpiece");
+    for (int i = 0; i < 3; i++) {
+        string cmt = comments[rand() % comments.size()];
+        m1.addReview(cmt);
+    }
+
     m1.printReviews();
     return 0;
 }
